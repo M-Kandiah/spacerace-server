265266521d40d37103ffd8288683b8ef2fca92c6
MathusanKandiah <mathusankandiah@hotmail.com>
2021-10-25 10:32:12 +0100

    register working

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -8,7 +8,7 @@ async function create(req, res) {
   try {
       const salt = await bcrypt.genSalt();
       const hashed = await bcrypt.hash(req.body.password, salt);
-      await User.create(req.body.username, hashed );
+      await User.create(req.body.username, hashed);
       res.status(201).json({ message: "User has been created successfully" });
   } catch (err) {
     res.status(500).json({ err });
--- db/seeds.js
+++
@@ -1,2 +0,0 @@
-const db = connect("mongodb://localhost:27017/users");
-//idk how mongo works without docker so empty
