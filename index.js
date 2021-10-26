f37bc0112ce19b377ce3ea7292da7331aff81f28
MathusanKandiah <mathusankandiah@hotmail.com>
2021-10-26 12:30:01 +0100

    socket started

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const app = require('./server');
-const { MongoClient } = require('mongodb');
 const mongoose = require('mongoose')
 require('dotenv').config({ path: __dirname + "./env" });
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const connectionUrl = process.env.CONNECTION_URL;
+const {instrument} = require('@socket.io/admin-ui')
+
+const server = require('http').createServer(app)
+const io = require('socket.io')(server, {cors: {origin:["http://localhost:3000","https://admin.socket.io"]} })
+
+
+io.on('connection', socket => {
+    console.log(socket.id)
+    socket.on('join-room', room => {
+        socket.join(room)
+    })
+
+    socket.on('say-hi', (dogs)=> {
+        console.log(dogs)
+    })
+})
 
 mongoose.connect(connectionUrl)
-    .then(() => app.listen(port, () => console.log(`express shooting off at ${port}`)))
+    .then(() => server.listen(port, () => console.log(`express shooting off at ${port}`)))
     .catch((err) => console.log('err'))
 
+instrument(io, {auth:false})
 
 
-
+// socket.emit is in join room button join and create
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 
-const server = express();
-server.use(cors());
-server.use(express.json());
+const app = express();
+app.use(cors());
+app.use(express.json());
 
 // importing routes
 const auth = require('./routes/authRoute');
-server.use('/auth', auth);
+app.use('/auth', auth);
 
 const user = require('./routes/userRoute');
-server.use('/users', user);
+app.use('/users', user);
 
-server.get('/', (req, res) => {res.send("Welcome to SpaceRace!")}); //name change pls this is so lame
+app.get('/', (req, res) => {res.send("Welcome to SpaceRace!")}); //name change pls this is so lame
 
-module.exports = server;
+module.exports = app;
