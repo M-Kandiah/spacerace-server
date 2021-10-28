c847a49703918254e35ea3a495119ab4c25ebcca
ria10 <ria1099@gmail.com>
2021-10-28 10:28:52 +0100

    adding emits for updating user list and added auth tests

--- index.js
+++ index.js
@@ -5,18 +5,21 @@ const port = process.env.PORT || 3001;
 const connectionUrl = process.env.CONNECTION_URL;
 const {instrument} = require('@socket.io/admin-ui')
 
+const {Users} = require('./models/users')
+
 const server = require('http').createServer(app)
 const io = require('socket.io')(server, {cors: {origin:["http://localhost:3000","https://admin.socket.io","https://brainifyquiz.netlify.app"]} })
-
+let users = new Users()
 
 io.on('connection', socket => {
     
     socket.on("join-room", (roomId, user) => {
         console.log(`Server : Someone joined room ${roomId}`);
-        console.log(io.sockets.adapter.rooms)
-        console.log(user)
         socket.join(roomId);
         io.in(roomId).emit("joined-room", roomId, user);
+        users.removeUser(socket.id);
+        users.addUser(socket.id, user, roomId);
+        io.to(roomId).emit('updateUsersList', users.getUserList(roomId))
       });
 
       socket.on("start-game", (room, url) => {
